05c90ef9cae8b779dd8ea55022879206f0098293
Jim Chen <theyee0@gmail.com>
2025-10-01 21:27:19 -0400

    Implemented addition, subtraction, comparison
    Shallow copy implemented for ease of implementation
    for other functions

--- apml.h
+++ apml.h
@@ -12,6 +12,10 @@
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
 #endif
 
+#ifndef MIN
+#define MAX(A, B) ((A) < (B) ? (A) : (B))
+#endif
+
 typedef unsigned int __apml_unit;
 
 struct apml_struct {
@@ -82,9 +86,31 @@ apml_t apml_set_scale(apml_t r, const int scale) {
         return r;
 }
 
-apml_t apml_c_mod(apml_t r, const apml_t a, const int c) {
-        /* Not yet Implemented */
-        exit(1);
+apml_t apml_shallow_copy(apml_t r, const apml_t a) {
+        r->scale = a->scale;
+        r->size = a->size;
+        r->num = a->num;
+        r->alloc = a->alloc;
+
+        r->sign = a->sign;
+
+        return r;
+}
+
+int apml_c_mod(const apml_t a, const int c) {
+        int i;
+        int m;
+        int t;
+
+        m = 0;
+        t = 1;
+        for (i = a->scale; i < a->size; i++) {
+                m += ((a->num[i] % c) * (t % c)) % c;
+                t *= a->base;
+                t %= c;
+        }
+
+        return m;
 }
 
 apml_t apml_c_div(apml_t r, const apml_t a, const int c) {
@@ -115,16 +141,171 @@ apml_t apml_c_mult(apml_t r, const apml_t a, const int c) {
         return r;
 }
 
-apml_t apml_set_base(apml_t r, const int base) {
+apml_t apml_set_base(apml_t r, apml_t a, const int base) {
+        /* Not yet implemented */
         return -1;
+}
+
+int apml_cmp(const apml_t a, const apml_t b) {
+        int i;
+
+        int av;
+        int bv;
+
+        if (a->base != b->base) {
+                return -1;
+        }
+
+        if (a->size - a->scale != b->size - b->scale) {
+                return (a->size - a->scale > b->size - b->scale) ? 1 : -1;
+        } else {
+                for (i = 1; i <= a->size - a->scale; i++) {
+                        av = a->num[a->size - i];
+                        bv = b->num[b->size - i];
+
+                        if (av > bv) {
+                                return 1;
+                        } else if (av < bv) {
+                                return -1;
+                        }
+                }
+
+                for (i = 1; i < MIN(a->scale, b->scale); i++) {
+                        av = a->num[a->scale - i];
+                        bv = b->num[b->scale - i];
+
+                        if (av > bv) {
+                                return 1;
+                        } else if (av < bv) {
+                                return -1;
+                        }
+                }
+
+                for (i = MIN(a->scale, b->scale); i < a->scale; i++) {
+                        av = a->num[a->scale - i];
+
+                        if (av > 0) {
+                                return 1;
+                        }
+                }
+
+                for (i = MIN(a->scale, b->scale); i < b->scale; i++) {
+                        bv = b->num[b->scale - i];
+
+                        if (bv > 0) {
+                                return 1;
+                        }
+                }
+        }
+
+        return 0;
 }
 
 apml_t apml_add(apml_t r, const apml_t a, const apml_t b) {
-        return -1;
+        int i;
+        int carry = 0;
+        long long tmp;
+
+        struct apml_struct t;
+
+        if (a->base != b->base || a->scale != b->scale) {
+                return NULL;
+        }
+
+        if (a->sign != b->sign) {
+                apml_shallow_copy(&t, b);
+                t.sign = a->sign;
+
+                return apml_sub(r, a, &t);
+        }
+
+        r->base = a->base;
+        r->sign = a->sign;
+        apml_set_scale(r, a->scale);
+        apml_reallocate(r, MAX(a->size, b->size) + 1);
+
+        for (i = 0; i < MIN(a->size, b->size); i++) {
+                tmp = carry;
+                tmp += a->num[i];
+                tmp += b->num[i];
+
+                r->num[i] = tmp % r->base;
+                carry = tmp / r->base;
+        }
+
+        if (a->size > b->size) {
+                r->size = a->size;
+                for (i = b->size; i < a->size; i++) {
+                        tmp = carry;
+                        tmp += a->num[i];
+
+                        r->num[i] = tmp % r->base;
+                        carry = tmp / r->base;
+                }
+        } else {
+                r->size = b->size;
+                for (i = a->size; i < b->size; i++) {
+                        tmp = carry;
+                        tmp += b->num[i];
+
+                        r->num[i] = tmp % r->base;
+                        carry = tmp / r->base;
+                }
+        }
+
+        if (carry > 0) {
+                r->num[r->size++] = carry;
+        }
+
+        return r;
 }
 
-apml_t apml_sub(apml_t r, apml_t a, apml_t b) {
-        return -1;
+apml_t apml_sub(apml_t r, const apml_t a, const apml_t b) {
+        int i;
+        int carry = 0;
+        long long tmp;
+
+        struct apml_struct t;
+
+        if (a->base != b->base || a->scale != b->scale) {
+                return NULL;
+        }
+
+        if (a->sign != b->sign) {
+                apml_shallow_copy(&t, b);
+                t.sign = a->sign;
+
+                return apml_add(r, a, &t);
+        }
+
+        apml_realloc(r, MAX(a->size, b->size));
+        r->base = a->base;
+
+        if (apml_cmp(a, b) < 0) {
+                apml_sub(r, b, a);
+        } else {
+                for (i = 0; i < MIN(a->size, b->size); i++) {
+                        r->num[i] = a->num[i] - b->num[i] - carry;
+                        if (r->num[i] < 0) {
+                                r->num[i] += r->base;
+                                carry = 1;
+                        } else {
+                                carry = 0;
+                        }
+                }
+
+                while (carry > 0) {
+                        r->num[i] = a->num[i] - carry;
+                        if (r->num[i] < 0) {
+                                r->num[i] += r->base;
+                                carry = 1;
+                        } else {
+                                carry = 0;
+                        }
+                }
+        }
+
+        return r;
 }
 
 apml_t apml_mult(apml_t r, apml_t a, apml_t b) {
