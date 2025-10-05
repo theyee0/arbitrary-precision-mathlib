cf2f065de90663aeb26ca554324d90bcd7b35333
Jim Chen <theyee0@gmail.com>
2025-10-05 12:32:37 -0400

    Add comments documenting program logic

--- inc/apml.h
+++ inc/apml.h
@@ -70,6 +70,7 @@ apml_t apml_realloc(apml_t r, const int n) {
                 r->num = realloc(r->num, n * sizeof(*r->num));
         }
 
+        /* Ensure that allocation was successful */
         if (r->num == NULL) {
                 apml_h_oom();
         }
@@ -87,6 +88,7 @@ apml_t apml_init(apml_t r, const int base) {
 
         r->num = malloc(r->alloc * sizeof(*r->num));
 
+        /* Ensure that allocation was successful */
         if (r->num == NULL) {
                 apml_h_oom();
         }
@@ -110,7 +112,12 @@ apml_t apml_free(apml_t r) {
 apml_t apml_fromstr(apml_t r, const int base, const char *s) {
         int i;
         int l = strlen(s);
-        int scale = l - (int)(strchr(s, '.') - s) - 1;
+        int scale = l - (int)(strchr(s, '.') - s) - 1; /* Scale of `s` */
+
+        /* If invalid base provided */
+        if (base < 1 || 10 < base) {
+                return NULL;
+        }
 
         apml_realloc(r, l);
         apml_set_scale(r, scale);
@@ -131,11 +138,24 @@ char *apml_tostr(apml_t r, char **s) {
         int i;
         int l = 0;
 
+        /* If invalid base provided */
+        if (r->base < 1 || 10 < r->base) {
+                *s = NULL;
+                return NULL;
+        }
+
         *s = malloc((r->size + 2) * sizeof(**s));
 
+        /* Ensure successful allocation */
+        if (*s == NULL) {
+                apml_h_oom();
+        }
+
+        /* Add digits to string */
         for (i = r->size - 1; i >= 0; i--) {
                 (*s)[l++] = r->num[i] + '0';
 
+                /* At the scale character, add decimal point */
                 if (i == r->scale) {
                         (*s)[l++] = '.';
                 }
@@ -148,16 +168,19 @@ char *apml_tostr(apml_t r, char **s) {
 
 /* Change the number of digits after decimal point */
 apml_t apml_set_scale(apml_t r, const int scale) {
-        int offset = scale - r->scale;
+        int offset = scale - r->scale; /* Number of digits to add to the scale */
 
         if (offset < 0) {
+                /* Shift digits down to lower precision */
                 memmove(r->num, r->num - offset, (r->size + offset) * sizeof(*r->num));
         } else {
+                /* Shift digits up and fill the new spaces with zeroes */
                 apml_realloc(r, r->size + offset);
                 memmove(r->num + offset, r->num, r->size * sizeof(*r->num));
                 memset(r->num, 0, offset * sizeof(*r->num));
         }
 
+        /* Update size and scale of number */
         r->size += offset;
         r->scale = scale;
 
@@ -198,6 +221,7 @@ int apml_c_mod(const apml_t a, const int c) {
         m = 0;
         t = 1;
         for (i = a->scale; i < a->size; i++) {
+                /* Calculate mod of current digit with ((a % c) * (b % c)) % c = (a * b) % c */
                 m += ((a->num[i] % c) * (t % c)) % c;
                 t *= a->base;
                 t %= c;
@@ -206,18 +230,21 @@ int apml_c_mod(const apml_t a, const int c) {
         return m;
 }
 
+/* Divide number by an integer and store the result back */
 apml_t apml_c_div_im(apml_t r, const int c) {
-        /* Not yet Implemented */
+        /* Not yet implemented; print values and exit */
         printf("%p %d\n", (void*)r, c);
         exit(1);
 }
 
+/* Divide number by an integer and store the result in a new variable */
 apml_t apml_c_div(apml_t r, const apml_t a, const int c) {
-        /* Not yet Implemented */
+        /* Not yet implemented; print values and exit */
         printf("%p %p %d\n", (void*)r, (void*)a, c);
         exit(1);
 }
 
+/* Multiply number by integer and store the result back */
 apml_t apml_c_mult_im(apml_t r, const int c) {
         int i;
         int carry = 0;
@@ -225,6 +252,7 @@ apml_t apml_c_mult_im(apml_t r, const int c) {
 
         apml_realloc(r, r->size + 1);
 
+        /* Use grade-school multiplication algorithm */
         for (i = 0; i < r->size; i++) {
                 tmp = c;
                 tmp *= r->num[i];
@@ -234,22 +262,26 @@ apml_t apml_c_mult_im(apml_t r, const int c) {
                 carry = tmp / r->base;
         }
 
-        if (carry > 0) {
-                r->num[r->size++] = carry;
+        /* Add extra digits if necessary */
+        while (carry > 0) {
+                apml_realloc(r, r->size + 1);
+                r->num[r->size++] = carry % r->base;
+                carry /= r->base;
         }
 
         return r;
 }
 
+/* Multiply number by an integer and store the result in a new variable */
 apml_t apml_c_mult(apml_t r, const apml_t a, const int c) {
         apml_copy(r, a);
 
         return apml_c_mult_im(r, c);
 }
 
+/* Change the base of a number */
 apml_t apml_set_base(apml_t r, const apml_t a, const int base) {
         int i, j, m;
-        apml_t t = malloc(sizeof(*t));
         apml_t d = malloc(sizeof(*d));
         int tmp;
 
@@ -270,14 +302,15 @@ apml_t apml_set_base(apml_t r, const apml_t a, const int base) {
                 r->num[r->size++ + r->scale] = tmp;
         }
 
-        d->scale = a->scale + 1; /* Higher scale to minimize roundoff error */
-        apml_realloc(d, d->scale + 1);
+        apml_init(d, base);
+        apml_set_scale(d, a->scale + 1); /* Higher scale to minimize roundoff error */
+        apml_realloc(d, d->scale + 2);
         d->size = d->scale + 1;
-        d->base = base;
 
         /* Compute fractional component */
         for (i = 0; i < a->scale; i++) {
                 d->num[d->size - 1] = a->num[i];
+
                 for (j = 0; j < a->scale - i - 1; j++) {
                         apml_c_div_im(d, base);
                 }
@@ -285,22 +318,29 @@ apml_t apml_set_base(apml_t r, const apml_t a, const int base) {
                 apml_add_im(r, d);
         }
 
+        apml_free(d);
+        free(d);
+
         return r;
 }
 
+/* Standard C comparison function returning -1, 0, or 1 after comparing a and b */
 int apml_cmp(const apml_t a, const apml_t b) {
         int i;
 
-        int av;
-        int bv;
+        int av; /* Value of a given digit of a */
+        int bv; /* Value of a given digit of b */
 
+        /* Assert that the bases are the same; TODO: Change scale to gracefully compare */
         if (a->base != b->base) {
-                return -1;
+                exit(-1);
         }
 
         if (a->size - a->scale != b->size - b->scale) {
+                /* If one number is clearly greater than the other by power*/
                 return (a->size - a->scale > b->size - b->scale) ? 1 : -1;
         } else {
+                /* Compare integer parts */
                 for (i = 1; i <= a->size - a->scale; i++) {
                         av = a->num[a->size - i];
                         bv = b->num[b->size - i];
@@ -312,6 +352,7 @@ int apml_cmp(const apml_t a, const apml_t b) {
                         }
                 }
 
+                /* Compare fractional parts */
                 for (i = 1; i < MIN(a->scale, b->scale); i++) {
                         av = a->num[a->scale - i];
                         bv = b->num[b->scale - i];
@@ -323,6 +364,7 @@ int apml_cmp(const apml_t a, const apml_t b) {
                         }
                 }
 
+                /* If a has more decimal places, check if any of them are nonzero */
                 for (i = MIN(a->scale, b->scale); i < a->scale; i++) {
                         av = a->num[a->scale - i];
 
@@ -331,6 +373,7 @@ int apml_cmp(const apml_t a, const apml_t b) {
                         }
                 }
 
+                /* If b has more decimal places, check if any of them are nonzero */
                 for (i = MIN(a->scale, b->scale); i < b->scale; i++) {
                         bv = b->num[b->scale - i];
 
@@ -343,6 +386,7 @@ int apml_cmp(const apml_t a, const apml_t b) {
         return 0;
 }
 
+/* Add two numbers and store the result back */
 apml_t apml_add_im(apml_t r, const apml_t a) {
         int i;
         int carry = 0;
@@ -350,10 +394,12 @@ apml_t apml_add_im(apml_t r, const apml_t a) {
 
         apml_t t = NULL;
 
+        /* Ensure that the numbers can be compared; TODO: Add graceful handling */
         if (r->base != a->base || r->scale != a->scale) {
                 return NULL;
         }
 
+        /* If the signs are different, treat as subtraction */
         if (r->sign != a->sign) {
                 t = malloc(sizeof(*t));
                 apml_shallow_copy(t, a);
@@ -372,6 +418,7 @@ apml_t apml_add_im(apml_t r, const apml_t a) {
 
         apml_realloc(r, MAX(r->size, a->size) + 1);
 
+        /* Add common digits */
         for (i = 0; i < MIN(r->size, a->size); i++) {
                 tmp = carry;
                 tmp += r->num[i];
@@ -381,12 +428,14 @@ apml_t apml_add_im(apml_t r, const apml_t a) {
                 carry = tmp / r->base;
         }
 
+        /* Add remaining digits if the numbers differ in length */
         if (r->size > a->size) {
                 i = a->size;
                 while (carry > 0) {
                         r->num[i] += carry;
                         carry = r->num[i] / r->base;
                         r->num[i] %= r->base;
+                        i++;
                 }
 
                 r->size = MAX(i + 1, r->size);
@@ -401,30 +450,36 @@ apml_t apml_add_im(apml_t r, const apml_t a) {
 
                 r->size = a->size;
 
-                if (carry > 0) {
-                        r->num[r->size++] = carry;
+                while (carry > 0) {
+                        r->num[r->size] += carry;
+                        carry = r->num[r->size] / r->base;
+                        r->num[r->size] %= r->base;
+                        r->size++;
                 }
         }
 
         return r;
 }
 
+/* Add numbers and store the result in a new variable */
 apml_t apml_add(apml_t r, const apml_t a, const apml_t b) {
         apml_copy(r, a);
         return apml_add_im(r, b);
 }
 
-/* TODO: Fix */
+/* Subtract two numbers and store the result back */
 apml_t apml_sub_im(apml_t r, const apml_t a) {
         int i;
         int carry = 0;
         int tmp;
         apml_t t = NULL;
 
+        /* Ensure that the numbers can be compared; TODO: Add graceful handling */
         if (r->base != a->base || r->scale != a->scale) {
                 return NULL;
         }
 
+        /* If the signs are different, treat as addition */
         if (r->sign != a->sign) {
                 t = malloc(sizeof(*t));
                 apml_shallow_copy(t, a);
@@ -444,6 +499,7 @@ apml_t apml_sub_im(apml_t r, const apml_t a) {
         apml_realloc(r, MAX(a->size, r->size));
         r->base = a->base;
 
+        /* Swap numbers around if the first number is less than the second */
         if (apml_cmp(r, a) < 0) {
                 t = malloc(sizeof(*t));
                 apml_init(t, r->base);
@@ -461,6 +517,7 @@ apml_t apml_sub_im(apml_t r, const apml_t a) {
                         return NULL;
                 }
         } else {
+                /* Subtract common digits */
                 for (i = 0; i < MIN(a->size, r->size); i++) {
                         tmp = r->num[i] - a->num[i] - carry;
                         if (tmp < 0) {
@@ -473,8 +530,10 @@ apml_t apml_sub_im(apml_t r, const apml_t a) {
                         r->num[i] = tmp;
                 }
 
+                /* Sort out remaining borrows */
                 while (carry > 0) {
                         tmp = r->num[i] - carry;
+
                         if (tmp < 0) {
                                 tmp += r->base;
                                 carry = 1;
@@ -489,20 +548,25 @@ apml_t apml_sub_im(apml_t r, const apml_t a) {
         return r;
 }
 
+/* Subtract two variables and store the result back */
 apml_t apml_sub(apml_t r, const apml_t a, const apml_t b) {
         apml_copy(r, a);
 
         return apml_sub_im(r, b);
 }
 
+/* Multiply two numbers and store the result in a new variable */
 apml_t apml_mult(apml_t r, apml_t a, apml_t b) {
+        /* Not yet implemented; print values and exit */
         printf("%p %p %p\n", (void*)r, (void*)a, (void*)b);
-        return NULL;
+        exit(1);
 }
 
+/* Divide two numbers and store the result in a new variable */
 apml_t apml_div(apml_t r, apml_t a, apml_t b) {
+        /* Not yet implemented; print values and exit */
         printf("%p %p %p\n", (void*)r, (void*)a, (void*)b);
-        return NULL;
+        exit(1);
 }
 
 #endif
